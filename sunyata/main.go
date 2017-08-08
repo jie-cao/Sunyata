14c21c896c9e9f9c3b6ccc92c54465c9dd6d86c1
Jie Cao <jicao@microsoft.com>
2017-08-08 15:37:22 -0700

    initial commit

--- sunyata/main.go
+++ sunyata/main.go
@@ -6,6 +6,6 @@ import "sunyata/core/web"
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("开始")
-	var newRoute = web.Route{Controller:"Main", Action:"Index"}
+	var newRoute web.Route = web.Route{Controller:"Main", Action:"Index"}
 	fmt.Println(newRoute.Controller)
 }
