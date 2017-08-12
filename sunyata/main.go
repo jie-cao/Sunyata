c4d835d152c8bf586139f777c5a0fcd407ffbc21
Jie Cao <jicao@microsoft.com>
2017-08-11 17:43:58 -0700

    register routes

--- sunyata/main.go
+++ sunyata/main.go
@@ -10,6 +10,8 @@ func main() {
 	//http.HandleFunc("/", route.DispatchRoute) //设置访问的路由
 
 	router := route.NewRouter()
+	RegisterRoutes(router)
+	
 	err := http.ListenAndServe(":9090", router) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
